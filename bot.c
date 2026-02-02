4ca60320a9f1bc921c72de75d48c639369037df4
DaCurse <dacurse@users.noreply.github.com>
2026-02-03 01:30:39 +0200

    update presence again

--- bot.c
+++ bot.c
@@ -115,14 +115,9 @@ static void bot_send_identify(MuseBot *bot) {
     int64_t now = (int64_t)time(NULL);
     ActivityData activities[] = {
         {
-            .name = "to your music links",
+            .name = "your music links",
             .type = ACTIVITY_LISTENING,
             .created_at = now,
-            .url = NULL,
-            .buttons = {{
-                .label = "Check me out on GitHub!",
-                .url = "https://github.com/DaCurse/muse/",
-            }},
         },
     };
 
